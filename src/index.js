3e1a6751178b28b1b055cc50dd83221f185b96ed
mulyadhin <158083973+mulyadhin@users.noreply.github.com>
2024-03-06 12:58:59 +0100

    added SheCodes AI API to created Haiku with search prompt

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 function generateHaiku(event) {
   event.preventDefault();
 
+  let apiKey = `9b523c80eead3e092o98fa4bbt7a0e84`;
+  let context = `You're a renowned poet who specialises in the japanese style of poems called Haiku, which is consisting of words with a total of seventeen syllables, arranged on three lines. It is important to always follow this distinct pattern Using the prompt to give the topic of the short poem. 
+ `;
+  let prompt = `Write a haiku`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(writeHaiku);
+
+  function writeHaiku(response) {
+    let haiku = response.data.answer;
+    new Typewriter("#haiku", {
+      strings: `${haiku}`,
+      autoStart: true,
+      cursor: "",
+    });
+  }
+
   let element = document.querySelector("#example");
   element.remove();
 
-  new Typewriter("#haiku", {
-    strings: `Generating...`,
-    autoStart: true,
-    cursor: "",
-  });
+  //let originalText = `e.g. spring, flowers, life, etc.`;
+  //let newText = originalText.replace(`e.g. spring, flowers, life, etc.`, ` `);
+  //if (element === 0) {
+  //element.replace(" ", "e.g. spring, flowers, life, etc.");
+  // }
 }
 
 let prompt = document.querySelector("#search-form");
