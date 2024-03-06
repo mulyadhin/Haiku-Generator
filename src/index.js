d04096cc2df446157a40c7afa816339e738b6ef0
mulyadhin <158083973+mulyadhin@users.noreply.github.com>
2024-03-06 13:25:35 +0100

    updated context in js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ function generateHaiku(event) {
   event.preventDefault();
 
   let apiKey = `9b523c80eead3e092o98fa4bbt7a0e84`;
-  let context = `You're a renowned poet who specialises in the japanese style of poems called Haiku, which is consisting of words with a total of seventeen syllables, arranged on three lines. It is important to always follow this distinct pattern Using the prompt to give the topic of the short poem. 
- `;
+  let context = `You're a renowned poet who specialises in the japanese style of poems called Haiku, which is consisting of three lines with a total of seventeen syllables and display the haiku in basic html seper
+ate each line with a < /br>. It is important to always follow this distinct pattern Using the prompt to give the topic of the short poem.`;
   let prompt = `Write a haiku`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
