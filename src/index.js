e190dbf3b19576ebbd0cd6fa4b7d5c76c6783e2a
mulyadhin <158083973+mulyadhin@users.noreply.github.com>
2024-03-13 15:46:13 +0100

    fixed error in js and added a loading sign

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ function generateHaiku(event) {
 
   let apiKey = `9b523c80eead3e092o98fa4bbt7a0e84`;
   let context = `You're a renowned poet who specialises in the japanese style of poems called Haiku, which is consisting of three lines with a total of seventeen syllables. display the haiku in basic html
-   IMPORTANT is to seperate each line with a < /br>. ALWAYS follow this distinct pattern Using the prompt to give the topic of the short poem.`;
+  ALWAYS follow this distinct pattern Using the prompt to give the topic of the short poem. Write the heading with 30px`;
   let prompt = `Write a haiku`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(writeHaiku);
 
   function writeHaiku(response) {
+    loading.classList.add("hidden");
     let haiku = response.data.answer;
     new Typewriter("#haiku", {
       strings: `${haiku}`,
@@ -20,6 +21,9 @@ function generateHaiku(event) {
 
   let element = document.querySelector("#example");
   element.classList.add("hidden");
+
+  let loading = document.querySelector("#loader");
+  loading.classList.remove("hidden");
 }
 
 let prompt = document.querySelector("#search-form");
